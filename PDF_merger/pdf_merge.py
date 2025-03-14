692b9307d456505d05d2dd2e43ba386d8a731b86
JayO <bluejayvrstudio@gmail.com>
2025-03-14 15:59:41 -0500

    just read file names in order; revised some comments

--- PDF_merger/pdf_merge.py
+++ PDF_merger/pdf_merge.py
@@ -1,17 +1,18 @@
-from pypdf import PdfWriter
+import os # We'll just use it to list input items
+from pypdf import PdfWriter # https://github.com/py-pdf/pypdf
 
-# pdf file names; get input pdf's from ./1_input/
-pdf_files = [f"./1_input/{i}.pdf" for i in range(1, 4)]
+# pdf file names; get input pdf's from "./1_input/"
+pdf_files = [f"./1_input/{filename}" for filename in os.listdir("./1_input")]
 print(pdf_files)
 
-# instantiate (get handle to) PdfMerger
+# Instantiate PdfMerger
 merger = PdfWriter()
 
-# append all PDFs
+# Append all PDFs
 for pdf in pdf_files:
     merger.append(pdf)
 
-# write output pdf to ./2_output/
+# Write output pdf to ./2_output/
 merger.write("./2_output/output.pdf")
 merger.close()
 
